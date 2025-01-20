82b8cbf6be933e39d132aa71a0c23c38b39529de
gzucca <gonzalozucca_4@outlook.com>
2025-01-20 10:42:06 -0300

    fixed typecheck errors

--- apps/expo/src/app/index.tsx
+++ apps/expo/src/app/index.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Button, Pressable, Text, TextInput, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Link, Stack } from "expo-router";
+import { Link } from "expo-router";
 import { FlashList } from "@shopify/flash-list";
 import React from 'react';
 import type { RouterOutputs } from "~/utils/api";
@@ -126,7 +126,7 @@ export default function Index() {
   return (
     <SafeAreaView className="bg-background">
       {/* Changes page title visible on the header */}
-      <Stack.Screen options={{ title: "Home Page" }} />
+      {/* <Stack.Screen options={{ title: "Home Page" }} /> */}
       <View className="h-full w-full bg-background p-4">
         <Text className="pb-2 text-center text-5xl font-bold text-foreground">
           Create <Text className="text-primary">T3</Text> Turbo
--- apps/expo/src/app/post/[id].tsx
+++ apps/expo/src/app/post/[id].tsx
@@ -1,5 +1,5 @@
 import { SafeAreaView, Text, View } from "react-native";
-import { Stack, useGlobalSearchParams } from "expo-router";
+import { useGlobalSearchParams } from "expo-router";
 
 import { api } from "~/utils/api";
 
@@ -12,12 +12,12 @@ export default function Post() {
 
   return (
     <SafeAreaView className="bg-background">
-      <Stack.Screen options={{ title: data.title }} />
+      {/* <Stack.Screen options={{ title: data.title }} /> */}
       <View className="h-full w-full p-4">
         <Text className="py-2 text-3xl font-bold text-primary">
-          {data.title}
+          {/* {data.title} */}
         </Text>
-        <Text className="py-4 text-foreground">{data.content}</Text>
+        {/* <Text className="py-4 text-foreground">{data.content}</Text> */}
       </View>
     </SafeAreaView>
   );
